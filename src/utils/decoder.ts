6286aed5f862f6a7b2e069c1f0620a975fd96aaa
Anderson Joseph <andersonjoseph@mailfence.com>
2023-07-17 18:50:47 -0400

    update object decoder

--- src/utils/decoder.ts
+++ src/utils/decoder.ts
@@ -1,4 +1,5 @@
 import Hashids from 'hashids';
+import utils from './index';
 
 type DecoderOptions = {
   readonly hashids: Hashids;
@@ -16,8 +17,16 @@ export class Decoder {
     const keys = Object.keys(outputObject);
 
     for (const key of keys) {
+      const currentValue = outputObject[key];
+
       if (this.options.idRegexp.test(key)) {
-        outputObject[key] = this.decodeString(String(outputObject[key]));
+        outputObject[key] = this.options.hashids.decode(
+          String(outputObject[key]),
+        )[0];
+      } else if (utils.isObject(currentValue)) {
+        outputObject[key] = this.decodeObject(currentValue);
+      } else if (utils.isArray(currentValue)) {
+        outputObject[key] = this.decodeArray(currentValue);
       }
     }
 
